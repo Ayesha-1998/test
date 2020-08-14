ea7b2764c0f99e68376acb759370d0a2c1d88f17
Ayesha <ayesha.cjc@gmail.com>
2020-08-14 22:37:13 +0530

    Updates

---
+++ precache-manifest.0949f266a8fc2b9d0d6910120f5aa24f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce50eaa09bf99de2c5c0b378cc841387",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "132b0a47217a0b615382",
+    "url": "/test/static/css/main.9ffea968.chunk.css"
+  },
+  {
+    "revision": "95f80ff3ae1b2f51047c",
+    "url": "/test/static/js/2.b4b3e657.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test/static/js/2.b4b3e657.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "132b0a47217a0b615382",
+    "url": "/test/static/js/main.3c77aa00.chunk.js"
+  },
+  {
+    "revision": "5426e9d6d2609ad63787",
+    "url": "/test/static/js/runtime-main.fa356a27.js"
+  }
+]);
